3e2c4df85e04de55b826da25efae874da6619dea
wlatic <jamesoh@gmail.com>
2024-05-20 01:40:43 -0700

    Update config_flow.py

--- custom_components/microair_easytouch/config_flow.py
+++ custom_components/microair_easytouch/config_flow.py
@@ -36,8 +36,8 @@ class MicroAirEasyTouchConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required("host"): str,
-                    vol.Required("port", default=5000): int,
+                    vol.Required("host", description="Host"): str,
+                    vol.Required("port", description="Port", default=5000): int,
                 }
             ),
             errors=errors,
@@ -80,6 +80,7 @@ class MicroAirEasyTouchOptionsFlowHandler(config_entries.OptionsFlow):
                 {
                     vol.Optional(
                         f"zone_{zone_id}_hvac_modes",
+                        description=f"HVAC Modes for {zone_name}",
                         default=self.config_entry.options.get(f"zone_{zone_id}_hvac_modes", ["heat", "cool", "fan_only", "off"]),
                     ): cv.multi_select({"heat": "Heat", "cool": "Cool", "fan_only": "Fan Only", "off": "Off"}),
                 }
